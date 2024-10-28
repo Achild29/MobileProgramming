b3e7f87af83800ae98878ad6f4bf400cc559fe46
Achild29 <wahyu.khoirurrizal@gmail.com>
2024-10-28 09:29:22 +0700

    Pertemuan 3: Membahas tentang MaterialApp, Scaffold, dan widget Text. tambahan juga terdapat widget column, row dan Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(List<String> args) {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      //MaterialApp adalah widget yang digunakan sebagai root(akar/inti) dari seluruh antarmuka aplikasi Flutter.
+      //didalam widget Material app terdapat parameter home yang isi nya adalah Scaffold
+      // Scaffold adalah sebuah widget dalam flutter yang menyediakan banyak widget seperti
+      // Drawer, SnackBar, BottomNavigationBar, FloatingActionButton, AppBar, dan lain-lain.
+      home: Scaffold(
+        //widget AppBar menambahkan panel seperti NavBar dibagian atas aplikasi
+        appBar: AppBar(
+          title: const Text(
+            //Widget Text untuk menampilkan Text Berupa String
+            'The Best Team in Premier League',
+            //parameter yang dimiliki widget Text: style untuk merubah tampilan text tersebut
+            style: TextStyle(
+              //berikut ini Parameter yg dimiliki oleh widget TextStyle;
+              color: Colors.white, //Merubah warna text menjadi putih
+              fontWeight:
+                  FontWeight.bold, //merubah text menjadi Bold / cetak tebal
+              fontSize: 18, //merubah ukuran dari text
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          //menambahkan warna untuk AppBar
+          backgroundColor: Colors.pink[400],
+        ),
+        //untuk parameter body, parameter ini biasanya akan diisi oleh widget yang
+        //akan memperluas atau menempati seluruh layar perangkat/tampilan
+        body: Container(
+          //disni saya menggunakan widget container untuk mewarnai seluruh layar/tampilan yang tersisa
+          color: Colors.pink[100],
+          child: const Column(
+            //disni saya mengisi paramater body dengan widget Column, dimana widget column bisa diisi lagi dengan banyak widget didalm childrennya
+            //dan tampilannya kebawah/vertikal.
+            mainAxisAlignment: MainAxisAlignment.center,
+            //diatas saya menggunakan parameter mainAxixsAligment dengan value center, yang membuat childrennya berada di tengah
+            children: [
+              Text(
+                'Manchester is',
+                style: TextStyle(fontSize: 50),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //diatas saya menggunakan parameter mainAxixsAligment dengan value spaceEvenly
+                //agar membuat childrennya mempunyai spase kosoong yg rata
+                children: [
+                  Text(
+                    'blue',
+                    style: TextStyle(
+                      color: Colors.lightBlue,
+                      fontWeight: FontWeight.w200, //agar terlihat lebih tipis
+                      decoration: TextDecoration.lineThrough,
+                      //diatas untuk menambahkan garis tengah pada text
+                      decorationColor: Colors.blue,
+                      //memberi warna biru pada garis tengah
+                      decorationThickness: 2,
+                      //memberikan ketebalan
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'Red',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
